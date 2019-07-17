a62cd512e8e6e438f14bbba5f74dda009515c8e7
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2019-07-17 08:04:26 +0200

    error handled

--- src/App.js
+++ src/App.js
@@ -2,52 +2,53 @@ import React from 'react';
 import './App.scss';
 import Form from './components/Form';
 import Weather from './components/Weather';
+import Backgound from './components/Background';
 
 const API_KEY = 'db88615648a49ecbd4abb035c15f3f88';
+const initialState = {
+  city: "City",
+  temperature: "0",
+  description: "",
+  error: ""
+}
 
 class App extends React.Component {
-  state = {
-    city: '',
-    temperature: '',
-    description:'',
-    error: ''
+  state = {...initialState}
+
+  componentDidMount() {
+    this.getWeather(undefined, 'the hague');
   }
 
-  getWeather = async (e) => {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
-    const data = await api_call.json();
-    if (city) {
-      console.log(data);
-      this.setState({
-        city: data.name,
-        temperature: data.main.temp,
-        description: data.weather[0].main,
-        error: ''
-      })
-    }
-    else {
-      this.setState({
-        city: "City",
-        temperature: "Temperature",
-        description: "",
-        error: "Please, type the name of a city"
-      })
-    }
+  getWeather = (e, givinCity) => {
+    e && e.preventDefault();
+    const city = e ? e.target.elements.city.value : givinCity;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+    .then((data)=> data.json())
+    .then((data)=> {
+      console.log('hello', data)
+      !data.message ?
+        this.setState({
+          city: data.name,
+          temperature: Math.round(data.main.temp),
+          description: data.weather[0].main,
+          error: ''
+        })
+       : this.setState({...initialState, error: data.message})
+    })
   }
-  
+
   render() {
     return (
       <div>
-        <h1>Hello Weather</h1>
-        <Form getWeather={this.getWeather} />
-        <Weather 
-          city={this.state.city}
-          temperature={this.state.temperature}
-          description={this.state.description}
-          error={this.state.error}
-        />
+        <Backgound weather={this.state.description}>
+          <Form getWeather={this.getWeather} />
+          <Weather 
+            city={this.state.city}
+            temperature={this.state.temperature}
+            description={this.state.description}
+            error={this.state.error}
+          />
+        </Backgound>
       </div>
     )
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,7 @@ import React from "react";
 const Weather = props => (
     <div>
         <h2>{props.city}</h2>
-        <h1>{props.temperature}</h1>
+        <h1>{props.temperature}&#176;</h1>
         <h5>Currently: {props.description}</h5>
         {props.error && <h5>{props.error}</h5>}
     </div>
